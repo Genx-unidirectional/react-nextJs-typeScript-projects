b1e602bd23fc65499010db68a6de0c6345a2b7ca
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-03-03 16:24:53 +0530

    tic tac toe using react hook

--- React-hooks/src/app/hookslearn/useEffectHook/tictactoe.tsx
+++ React-hooks/src/app/hookslearn/useEffectHook/tictactoe.tsx
@@ -1,4 +1,29 @@
-const tictactoe = () => {
-  return <div>tictactoe</div>;
+"use client";
+
+import { useState } from "react";
+
+const TicTacToe = () => {
+  const [board, setBoard] = useState<(number | undefined)[]>(
+    Array<number | undefined>(8)
+  );
+  const handleClick = () => {
+    console.log("click");
+  };
+  return (
+    <div className="grid grid-cols-3 grid-rows-3 gap-2">
+      {board.map((resp, idx) => {
+        const cellVal = resp !== undefined ? resp : " ";
+        return (
+          <div
+            key={idx}
+            className="h-12  rounded-sm w-12 text-3xl flex  justify-center items-center bg-white text-black"
+            onClick={handleClick}
+          >
+            {cellVal}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
-export default tictactoe;
+export default TicTacToe;
