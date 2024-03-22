6233442d78b91ac684a2f2f07368831bf0c7166d
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-03-22 22:23:30 +0530

    feat product context

---
+++ React-hooks/src/app/context/productProvider.tsx
@@ -0,0 +1,44 @@
+import { ReactElement, useState, createContext } from "react";
+
+export type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
+};
+const initState: ProductType[] = [
+  {
+    sku: "item0001",
+    name: "Widget",
+    price: 9.99,
+  },
+  {
+    sku: "item0002",
+    name: "Premium Widget",
+    price: 19.99,
+  },
+  {
+    sku: "item0003",
+    name: "Deluxe Widget",
+    price: 29.99,
+  },
+];
+export type UseProductType = {
+  products: ProductType[];
+};
+
+const initContextState = {
+  products: [],
+};
+
+const ProductContext = createContext<UseProductType>(initContextState);
+type ChildrenProp = {
+  children?: ReactElement | ReactElement[];
+};
+const ProductProvider = ({ children }: ChildrenProp) => {
+  const [products, setProduct] = useState<ProductType[]>(initState);
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
