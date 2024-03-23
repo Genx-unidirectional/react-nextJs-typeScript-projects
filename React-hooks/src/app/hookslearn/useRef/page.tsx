663a108a92eee67e642a5d062e0842e82150e59d
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-03-23 08:38:48 +0530

    feat useRef hook created at hookslearn

---
+++ React-hooks/src/app/hookslearn/useRef/page.tsx
@@ -0,0 +1,4 @@
+const Page = () => {
+  return <div>Use Ref Hook examples</div>;
+};
+export default Page;
--- React-hooks/src/app/ui/productCartUi/product.tsx
+++ React-hooks/src/app/ui/productCartUi/product.tsx
@@ -1,4 +1,4 @@
 const Product = () => {
-  return <div>Product</div>;
+  return <div></div>;
 };
 export default Product;
