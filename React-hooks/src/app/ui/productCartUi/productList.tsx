49fd8e7ceeff2eb9bd0eac0f119ecfdf65cbf098
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-03-27 20:17:41 +0530

    feat productList code

--- React-hooks/src/app/ui/productCartUi/productList.tsx
+++ React-hooks/src/app/ui/productCartUi/productList.tsx
@@ -1,13 +1,21 @@
 import UseProduct from "@/app/hooks/useProduct";
 import Product from "./product";
+import UseCart from "@/app/hooks/useCart";
+import { ReactElement } from "react";
 
 const ProductList = () => {
   const { products } = UseProduct();
+  const { dispatch, Reducer_Actions, cart } = UseCart();
+  let pageContent: ReactElement | ReactElement[] = <p>...Loading</p>;
+  if (products?.length) {
+    pageContent = products.map((product) => {
+      const inCart = cart.some((cartItem) => cartItem.sku === product.sku);
+      return <Product />;
+    });
+  }
   return (
     <main className="h-full p-1 text-black flex flex-col justify-center items-center ">
-      {products.map((product) => {
-        return <Product />;
-      })}
+      {pageContent}
     </main>
   );
 };
