312f80fcd52fa5cf52bae0ff0285da1020718b1a
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-04-22 17:10:43 +0530

    Feat hooks with types

---
+++ React-hooks/src/app/typedHooks/page.tsx
@@ -0,0 +1,8 @@
+function page() {
+  return (
+    <div className="bg-white text-black min-h-screen w-full flex flex-col justify-center items-center">
+      page
+    </div>
+  );
+}
+export default page;
